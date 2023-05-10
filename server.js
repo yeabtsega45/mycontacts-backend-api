8431417598dcc924b630d5dff00d011deeae11f2
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-10 15:49:38 +0300

    intializing port

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv").config;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.send("get all contacts");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
