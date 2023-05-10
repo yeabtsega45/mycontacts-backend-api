119ee625effc0bd66c3c80db57f0aa9df0e099e9
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-10 21:16:25 +0300

    creating multiple routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/", (req, res) => {
+router.route("/").get((req, res) => {
   res.status(200).json({ message: "get all contacts" });
+});
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "create contact" });
+});
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `get contact for ${req.params.id}` });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `update contact for ${req.params.id}` });
+});
+
+router.route("/").delete((req, res) => {
+  res.status(200).json({ message: `delete contact for ${req.params.id}` });
 });
 
 module.exports = router;
