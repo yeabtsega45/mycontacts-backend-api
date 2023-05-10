ca7ce7625d290acfe0469ce6682b17ce9b6e5d02
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-10 20:51:06 +0300

    creating routes

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const dotenv = require("dotenv").config;
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-  res.send("get all contacts");
-});
+app.use();
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
