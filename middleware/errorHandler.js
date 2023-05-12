55302805d2adbcd75a185467ef3bc1a30f7a4ca2
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-12 11:51:17 +0300

    renaming constants

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,3 +1,8 @@
-const errorHandler = (err, req, res, next) => {};
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case
+  }
+};
 
 module.exports = errorHandler;
