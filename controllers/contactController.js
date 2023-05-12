2f8243ac5a847c8c2b5c4e839e6268fe856d6c8e
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-12 11:20:33 +0300

    accepting body from the request

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,12 @@ const getContacts = (req, res) => {
 };
 
 const createContact = (req, res) => {
+  console.log(req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory !");
+  }
   res.status(201).json({ message: "create contact" });
 };
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv").config;
+const dotenv = require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
