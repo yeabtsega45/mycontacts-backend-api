56140528633166fff4008c4bfd30a0a797b3557e
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-12 12:10:23 +0300

    creating error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -9,7 +9,32 @@ const errorHandler = (err, req, res, next) => {
         message: err.message,
         stackTrace: err.stack,
       });
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.FORBIDDEN:
+      res.json({
+        title: "forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.NOT_FOUND:
+      res.json({
+        title: "not found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "server error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
     default:
+      console.log("No error, All good !");
   }
 };
 
