439cdd634c6cce3fec2711d8a96668f114fefad4
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-12 12:04:33 +0300

    switching error cases

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,15 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
-    case
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "validation failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    default:
   }
 };
 
