1fd6e42c5ccd1fca065fedb6bcc8564c31f2a1c0
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-29 00:17:13 +0300

    destructuring the model in the controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,7 +2,8 @@ const asyncHandler = require("express-async-handler");
 const Contact = require("../models/contactModel");
 
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 const createContact = asyncHandler(async (req, res) => {
@@ -12,7 +13,8 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory !");
   }
-  res.status(201).json({ message: "create contact" });
+  const contact = await Contact.create({ name, email, phone });
+  res.status(201).json(contact);
 });
 
 const getContact = asyncHandler(async (req, res) => {
