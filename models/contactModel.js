e7756ec145196316f22a6a69ffa840db5fea0fa8
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-28 23:38:05 +0300

    creating model for the contacts

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
-const contactSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "please add the contact name"],
+const contactSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please add the contact name"],
+    },
+    email: {
+      type: String,
+      required: [true, "please add the contact email"],
+    },
+    phone: {
+      type: String,
+      required: [true, "please add the contact phone"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "please add the contact email"],
-  },
-  phone: {
-    type: String,
-    required: [true, "please add the contact phone"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("contacts", contactSchema);
