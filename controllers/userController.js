18f32bba449a3631df921c5fd0444cace7c3d5d0
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-02 13:07:08 +0300

    user registration functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
     res.status(400);
     throw new Error("all fields are mandatory !");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("user already registered!");
   }
   res.status(200).json(contacts);
 });
