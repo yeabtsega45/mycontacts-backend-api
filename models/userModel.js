96b4d65c24c9e64cbc8b93a01c45e09557b2d39b
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-02 12:19:15 +0300

    mongoose schema for users

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Please add the username"],
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please add the username"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add the email"],
+      unique: [true, "email address already taken"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add the password"],
+    },
   },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Users", userSchema);
